54d2d7f4659c6c92de8407b1e46a10b5f52b41f6
TimDiekmann <TimDiekmann@users.noreply.github.com>
2021-08-21 20:54:55 +0000

    deploy: f56c236c74a833aaca1bf36e2324393ec6df6650

--- implementors/core/marker/trait.StructuralEq.js
+++ implementors/core/marker/trait.StructuralEq.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["tag_game"] = [{"text":"impl&lt;S, B&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"struct\" href=\"tag_game/struct.Agent.html\" title=\"struct tag_game::Agent\">Agent</a>&lt;S, B&gt;","synthetic":false,"types":["tag_game::agent::Agent"]}];
+implementors["tag_game"] = [{"text":"impl&lt;'sim, S, B&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"struct\" href=\"tag_game/struct.Agent.html\" title=\"struct tag_game::Agent\">Agent</a>&lt;'sim, S, B&gt;","synthetic":false,"types":["tag_game::agent::Agent"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
