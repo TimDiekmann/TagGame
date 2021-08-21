10e3654df6ec7e13e2e4bd0f69c096713f31d99c
TimDiekmann <TimDiekmann@users.noreply.github.com>
2021-08-21 11:40:28 +0000

    deploy: 21a14dc5935b0aee27090858ab683b3cffb40f50

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"tag_game":{"doc":"","t":[5],"n":["main"],"q":["tag_game"],"d":[""],"i":[0],"f":[[[]]],"p":[]}\
+}');
+if (window.initSearch) {window.initSearch(searchIndex)};
