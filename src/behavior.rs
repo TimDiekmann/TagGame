e258afc93f402cf35a7c2671c261e13e1c39f3a4
Tim Diekmann <tim.diekmann@3dvision.de>
2021-08-21 22:43:16 +0200

    Store agents as HashMap and add `Simulation::update()`

--- src/behavior.rs
+++ src/behavior.rs
@@ -8,8 +8,27 @@ pub trait Behavior: Sized {
     type World;
 
     /// Called when an agent is created to the world.
-    fn on_creation(&self, agent: &Agent<Self::State, Self>, world: &Self::World);
+    fn on_creation(&self, agent: &Agent<Self::State, Self>, world: &Self::World) {
+        let _ = agent;
+        let _ = world;
+    }
 
     /// Called when an agent is removed from the world.
-    fn on_deletion(&self, agent: &Agent<Self::State, Self>);
+    fn on_deletion(&self, agent: &Agent<Self::State, Self>) {
+        let _ = agent;
+    }
+
+    /// Called when the simulation is updated.
+    fn on_update<'sim>(
+        &'sim self,
+        id: u64,
+        state: &'sim mut Self::State,
+        world: &'sim Self::World,
+        population: impl Iterator<Item = Agent<'sim, Self::State, Self>>,
+    ) {
+        let _ = id;
+        let _ = state;
+        let _ = world;
+        drop(population);
+    }
 }
